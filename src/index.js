75e9785b02be861ad8b46c0e9304271f720509fd
joebanhong <joeanddaneil5@gmail.com>
2023-02-09 20:45:42 +0800

    feat: add global style and media query - global style 操作 - shop page rwd - directory page rwd - auth page rwd - checkout page rwd

--- src/index.js
+++ src/index.js
@@ -8,14 +8,16 @@ import { Elements } from "@stripe/react-stripe-js";
 import App from "./App";
 import { store, persistor } from "./store/store";
 
-import "./index.scss";
+// import "./index.scss";
 import { stripePromise } from "./utils/stripe/stripe.utils";
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'
+import { GlobalStyle } from "./global.style";
 
 const rootElement = document.getElementById("root");
 
 render(
   <React.StrictMode>
+    <GlobalStyle />
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
---
+++ src/routes/authentication/authentication.styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const AuthenticationContainer = styled.div`
+  display: flex;
+  width: 900px;
+  justify-content: space-between;
+  margin: 30px auto;
+
+  @media screen and (max-width: 800px) {
+    flex-direction: column;
+    justify-content: unset;
+    width: 100%;
+    grid-gap: 30px;
+  }
+`;
